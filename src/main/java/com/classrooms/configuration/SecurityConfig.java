5f5ea9996b9beaaa5647a533ac96aa533c70a1b8
A696746 <yussuf9@gmail.com>
2019-10-12 01:17:27 +0200

    commit 13

--- src/main/java/com/classrooms/configuration/SecurityConfig.java
+++ src/main/java/com/classrooms/configuration/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	        http .csrf().disable();
 	       // http.formLogin();
 	        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	        http.authorizeRequests().antMatchers("/login/**").permitAll();
+	       // http.authorizeRequests().antMatchers("/login/**").permitAll();
 	        //http.authorizeRequests().antMatchers(HttpMethod.GET,"/classes").hasAuthority("admin");
 	        http.authorizeRequests().antMatchers(HttpMethod.GET,"/classes").permitAll();
 	        http.authorizeRequests().antMatchers(HttpMethod.POST,"/classe").permitAll();

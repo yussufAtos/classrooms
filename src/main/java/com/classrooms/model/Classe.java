391904fc5e5b489861a4d79226a97d300241c22e
A696746 <yussuf9@gmail.com>
2019-10-13 01:27:38 +0200

    commit 27

--- src/main/java/com/classrooms/model/Classe.java
+++ src/main/java/com/classrooms/model/Classe.java
@@ -10,6 +10,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -25,7 +26,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Classe implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue
+	//@GeneratedValue
+	@GeneratedValue (strategy = GenerationType.IDENTITY) 
+
 	private int id;
 
 	private String classeName;

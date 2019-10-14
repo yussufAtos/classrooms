f140be05eaff8b2da5464e0ea1fffc5a22afe126
A696746 <yussuf9@gmail.com>
2019-10-14 12:31:46 +0200

    add user

--- src/main/java/com/classrooms/ClassroomsApplication.java
+++ src/main/java/com/classrooms/ClassroomsApplication.java
@@ -5,19 +5,47 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//import com.classrooms.dao.ClasseDao;
-import com.classrooms.model.Classe;
+import com.classrooms.dao.RoleDao;
+import com.classrooms.dao.UserDao;
+import com.classrooms.model.Role;
+import com.classrooms.model.UserApp;
+import com.classrooms.service.UserService;
 
 @SpringBootApplication
-public class ClassroomsApplication implements CommandLineRunner{
-//	@Autowired
-//	private ClasseDao classeDao;
+public class ClassroomsApplication implements CommandLineRunner {
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private RoleDao roleDao;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	private UserService userService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClassroomsApplication.class, args);
 	}
+
+	@Bean
+	public BCryptPasswordEncoder getBCPE() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	public void run(String... agr0) {
+
+		UserApp userApp = userDao.save(new UserApp("youssef", "youssef", 25));
+		String hashPwd = bCryptPasswordEncoder.encode("youssef");
+		Role r1 = new Role("admin");
+		Role r2 = new Role("user");
+		userApp.getRoles().add(r1);
+		userApp.getRoles().add(r2);
+		userService.save(userApp);
 	}
 
 }
